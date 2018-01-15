93976f3485c8139c25a7816e31fbda09be3518f6
shiwei.li <shiwei.li@hpe.com>
2018-01-15 18:31:44 +0000

    2018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.152018.1.15

--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,10 @@
-
-function clickOperition () {
-    var element = document.querySelector('#operition')
-    console.log(document.getElementById('operition').innerText);
-    if (element.innerText === 'Start Recording') {
-        element.querySelector('span').innerText = "Stop Recording"
-        element.querySelector('img').src='img/pause-button.png';
-    } else if (element.innerText === 'Stop Recording') {
-        element.querySelector('span').innerText = "Start Recording"
-        element.querySelector('img').src='img/play-button.png';
-    }
-}
-
+let bgDom = chrome.extension.getBackgroundPage();
+bgDom.checkStatus();
 function clickConfig () {
     chrome.runtime.openOptionsPage();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('operition').addEventListener('click', clickOperition);
-  document.querySelector('#config').addEventListener('click', clickConfig);
+    document.getElementById('operition').addEventListener('click', bgDom.clickOperition);
+    document.querySelector('#config').addEventListener('click', clickConfig);
 });
--- scripts/bg.js
+++ scripts/bg.js
@@ -7,11 +7,128 @@ chrome.runtime.onInstalled.addListener(function(detail){
   contextMessage:" From OneView Api Recording"});
 })
 
-function logURL(requestDetails) {
-  console.log("Loading: " );
-  console.log(1);
+var recordingStatus = {
+  startTime: null,
+  stopTime: null,
+  isStart : false,
+  actionTimer: function () {
+    if(this.isStart) {
+      this.startTime = new Date();
+    } else {
+      this.stopDate = new Date();
+    }
+  },
+
+  notific: function () {
+    let opt;
+  if(this.isStart) {
+    opt = {
+      type: "basic",
+    iconUrl:"popup/img/hpe.svg",
+    title:"Start Recording",
+  message: "",
+  contextMessage:" From OneView Api Recording"
+    }
+  } else {
+    opt = {
+      type: "basic",
+    iconUrl:"popup/img/hpe.svg",
+    title:"Stop Recording",
+  message: "",
+  contextMessage:" From OneView Api Recording"
+    }
+  }
+  chrome.notifications.create(opt);
+  },
+
+  recordingAipStart: function () {
+    chrome.webRequest.onBeforeRequest.addListener(
+      function(requestDetails){
+        if(requestDetails.method !== "GET") {
+          let buf = requestDetails.requestBody.raw[0].bytes;
+         arrayBufferToObj(buf).onload=function(){
+           console.log(1,JSON.parse(this.result));
+         }
+        }
+      },
+       {urls:["<all_urls>"]},
+       ["requestBody"]
+     );
+     
+     chrome.webRequest.onBeforeSendHeaders.addListener(
+       function(requestDetails){
+         if(requestDetails.method !== "GET") {
+           console.log(1.2, requestDetails);
+          console.log(2, requestDetails.requestHeaders);
+         }
+       },
+        {urls: ["<all_urls>"]},
+        ["requestHeaders"]
+      );
+     
+      chrome.webRequest.onCompleted.addListener(
+       function(requestDetails){
+         if(requestDetails.method !== "GET") {
+          console.log(3,requestDetails);
+         }
+       },
+        {urls: ["<all_urls>"]},
+        ["responseHeaders"]
+      );
+  }
 }
-logURL();
+
+function clickOperition () {
+  let popup = chrome.extension.getViews({type:"popup"});
+  let operition =  popup[0].document.querySelector("#operition");
+  if (recordingStatus.isStart) {
+    stopRecording(operition);
+  } else {
+     startRecording(operition);
+  }
+  recordingStatus.actionTimer();
+  recordingStatus.notific();
+}
+
+function checkStatus () {
+  let popup = chrome.extension.getViews({type:"popup"})[0];
+  chrome.tabs.query({
+    active:true,
+    lastFocusedWindow:true
+  },function(tabs){
+    let reg = /^https:\/\/15.114.*$/;
+    if (!reg.test(tabs[0].url)) {
+     popup.document.body.innerHTML="<h2>Sorry, This page seem to be not supported.<h2>";
+      return;
+    }
+   
+     let operition =  popup.document.querySelector("#operition");
+    if (!recordingStatus.isStart) {
+      operition.querySelector('span').innerText = "Start Recording"
+       operition.querySelector('img').src='img/play-button.png';
+   } else {
+       operition.querySelector('span').innerText = "Stop Recording"
+       operition.querySelector('img').src='img/pause-button.png';
+   }
+  })
+}
+
+function startRecording (element) {
+  recordingStatus.recordingAipStart ()
+  chrome.browserAction.setIcon({path: "recording.png"});
+  chrome.browserAction.setBadgeText({text: "||"});
+  element.querySelector('span').innerText = "Stop Recording"
+  element.querySelector('img').src='img/pause-button.png';
+  recordingStatus.isStart = !recordingStatus.isStart;
+}
+
+function stopRecording (element) {
+  chrome.browserAction.setIcon({path:"title.png"});
+  chrome.browserAction.setBadgeText({text: ""});
+  element.querySelector('span').innerText = "Start Recording"
+  element.querySelector('img').src='img/play-button.png';
+  recordingStatus.isStart = !recordingStatus.isStart;
+}
 
 function arrayBufferToObj(buf) {
   var blob = new Blob([buf]);
@@ -20,37 +137,3 @@ function arrayBufferToObj(buf) {
     return reader
 }
 
-
-chrome.webRequest.onBeforeRequest.addListener(
- function(requestDetails){
-   if(requestDetails.method !== "GET") {
-     let buf = requestDetails.requestBody.raw[0].bytes;
-    arrayBufferToObj(buf).onload=function(){
-      console.log(JSON.parse(this.result));
-    }
-   }
- },
-  {urls: ["https://15.114.113.141/*"]},
-  ["requestBody"]
-);
-
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(requestDetails){
-    if(requestDetails.method !== "GET") {
-      console.log(1.2, requestDetails);
-     console.log(2, requestDetails.requestHeaders);
-    }
-  },
-   {urls: ["https://15.114.113.141/*"]},
-   ["requestHeaders"]
- );
-
- chrome.webRequest.onCompleted.addListener(
-  function(requestDetails){
-    if(requestDetails.method !== "GET") {
-     console.log(3, requestDetails.requestBody);
-    }
-  },
-   {urls: ["https://15.114.113.141/*"]},
-   ["responseHeaders"]
- );
